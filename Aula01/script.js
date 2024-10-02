9240b1a4b10e13bf87c05849ea47d91716c3a611
andredisner <andredisner@gmail.com>
2024-10-01 21:14:34 -0300

    6

--- Aula01/script.js
+++ Aula01/script.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 
-    $("inpuut[name=cep]").mask("00000-000");
+    $("input[name=cep]").mask("00000-000");
     $("form").on("submit", function(event) {
         event.stopPropagation();
         event.stopDefault();
     });
 
-    $("input[name.cep]").on("change", function(event){
-
-    })
+    $("input[name=cep]").on("keyup", function(event) {
+        let cep = $("input[name=cep]").val();
+        alert(cep);
+    });
 });

f51a48e0bb35aa5c2193c31fea323a8f3da33d74
andredisner <andredisner@gmail.com>
2024-10-02 20:45:16 -0300

    6

--- Aula01/script.js
+++ Aula01/script.js
@@ -1,30 +1,38 @@
-$(document).ready(function() {
+        $(document).ready(function() {
 
-    $("input[name=cep]").mask("00000-000");
-    $("input[name=numero]").mask("#");
-    $("form").on("submit", function(event) {
-        event.stopPropagation();
-        event.stopDefault();
-    });
+            $("input[name=cep]").mask("00000-000");
+            $("input[name=numero]").mask("n",{
+                translation: {
+                    'n': {
+                        pattern: /[0-9]/,
+                        recursive: true
+                    }
+                }
+            });
+            
+            $("form").on("submit", function(event) {
+                event.stopPropagation();
+                event.stopDefault();
+            });
 
-    $("input[name=cep]").on("keyup", function(event) {
-        let cep = $("input[name=cep]").val();
-        cep = cep.replace("-","");
-        if(cep.length == 8) {
-            $("input[name=cep]").removeClass("is-invalid");
-            $.ajax("https://viacep.com.br/ws/" + cep + "/json")
-                .done(function(data) {
-                    let resposta = JSON.parse(data);
-                   if (!resposta.erro){
-                        $("input[name=rua]").val(resposta.logradouro);
-                        $("input[name=complemento]").val(resposta.complemento);
-                        $("input[name=bairro]").val(resposta.bairro);
-                        $("select[name=estado]").val(resposta.uf);
-                        $("input[name=cidade]").val(resposta.localidade);
-                        $("input[name=uf]").val(resposta.uf);
-                        $("input[name=ibge]").val(resposta.ibge);
-                   }
-                });
-        }
-    });
-});
+            $("input[name=cep]").on("keyup", function(event) {
+                let cep = $("input[name=cep]").val();
+                cep = cep.replace("-","");
+                if(cep.length == 8) {
+                    $("input[name=cep]").removeClass("is-invalid");
+                    $.ajax("https://viacep.com.br/ws/" + cep + "/json")
+                        .done(function(data) {
+                            let resposta = JSON.parse(data);
+                        if (!resposta.erro){
+                                $("input[name=rua]").val(resposta.logradouro);
+                                $("input[name=complemento]").val(resposta.complemento);
+                                $("input[name=bairro]").val(resposta.bairro);
+                                $("select[name=estado]").val(resposta.uf);
+                                $("input[name=cidade]").val(resposta.localidade);
+                                $("input[name=uf]").val(resposta.uf);
+                                $("input[name=ibge]").val(resposta.ibge);
+                        }
+                        });
+                }
+            });
+        });

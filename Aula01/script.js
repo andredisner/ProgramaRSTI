7e864aba85a03a30198eeab81e38a5932f0cf08a
andredisner <andredisner@gmail.com>
2024-10-03 21:45:30 -0300

    1

--- Aula01/script.js
+++ Aula01/script.js
@@ -13,7 +13,21 @@ $(document).ready(function() {
             }
         }
     });
+    $("input[name=telefone)").mask("(00) 00000-0000");
+    $('input[name=telefone)').on('keyup',function(){
+        let telefone = $("input[name=telefone)").val()
+        ;// Remove todos os caracteres que não são números.
+        telefone = telefone.replace(/[\D]/g,'');
+    
+    if (telefone.length < 11) {
+        mascara = '(00) 0000-00000';
+    } else { 
+        mascara = '(00) 00000-0000';
+    }
+    
+    $("input[name=telefone]").mask(mascara);
 
+});
     const urlEstados = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome';
 
     // Carregar os estados na inicialização
